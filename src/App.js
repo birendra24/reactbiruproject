19bd28de7be82c266860ab54c46d363f3ee117ad
biru <birendrakshetri90801@gmail.com>
2023-01-09 20:34:57 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import React from 'react'
-//import Resturant from './component/basics/Resturant';
-//import UseState from './component/ReactHooks/useState';
-//import UseEffect from './component/ReactHooks/useEffect';
-//import UseReducer from './component/ReactHooks/useReducer';
-//import Todo from './component/todoReact/todo';
-import Temp from './component/weather/temp';
+import React from "react";
+import Todo from "./components/Todo/Todo";
 
-const App  = () => {
+const App = () => {
   return (
     <>
-    {/* <Resturant /> */}
-    {/* <UseState /> */}
-    {/* <UseEffect /> */}
-    {/* <UseReducer /> */}
-    {/* < Todo /> */}
-    < Temp />
-
-    
+      <Todo />
     </>
-  )
-}
+  );
+};
 
-export default App 
+export default App;
